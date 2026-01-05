ac9e448737b3ea1cefc4aea9bf67b1acef3548d8
Niladri Banik <103855172+NILADRI-BANIK@users.noreply.github.com>
2026-01-05 21:03:35 +0530

    Time: 0 ms (100%), Space: 42.1 MB (51.01%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int fib(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        }
+
+        int i, ft = 0, st = 1, tt;
+        for (i = 1; i <= n; i++) {
+            tt = ft + st;
+
+            ft=st;
+            st = tt;
+        }
+        return ft;
+    }
+}
