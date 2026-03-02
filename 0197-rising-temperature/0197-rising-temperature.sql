e34a8849b22b4dd4a28167e0a16abde96df36894
Niladri Banik <103855172+NILADRI-BANIK@users.noreply.github.com>
2026-03-02 20:00:00 +0530

    Time: 470 ms (52.19%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,6 +1,7 @@
 # Write your MySQL query statement below
 SELECT W1.id FROM
 Weather AS W1 JOIN Weather AS W2
+# ON W1.recordDate=W2.recordDate+1
 ON DATEDIFF(W1.recordDate,W2.recordDate)=1
 WHERE W1.temperature>W2.temperature;
 # w1 = today
