ac324c6feb73bdd6c0a5b8013b9d50572a729a8a
Niladri Banik <103855172+NILADRI-BANIK@users.noreply.github.com>
2026-03-06 01:18:48 +0530

    Time: 598 ms (30.71%), Space: 0B (100%) - LeetHub

---
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -0,0 +1,18 @@
+# Write your MySQL query statement below
+#| pid | tiv_2015 | tiv_2016 | lat | lon |
+#+-----+----------+----------+-----+-----+
+#| 1   | 10       | 5        | 10  | 10  |
+#| 2   | 20       | 20       | 20  | 20  |BAND (SAME CITY)
+#| 3   | 10       | 30       | 20  | 20  |BAND (SAME CITY)
+#| 4   | 10       | 40       | 40  | 40  |
+#40+5=45.00
+SELECT ROUND(SUM(tiv_2016),2) AS tiv_2016 
+FROM Insurance
+WHERE tiv_2015 IN(
+    SELECT tiv_2015 FROM Insurance GROUP BY tiv_2015
+    HAVING COUNT(*)>1
+)
+AND(lat,lon)IN(
+    SELECT lat,lon FROM Insurance GROUP BY lat,lon 
+    HAVING COUNT(*)=1
+)
